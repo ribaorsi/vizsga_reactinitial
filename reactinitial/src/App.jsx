64452b5b23a9b3b79d6fd36f41541e72023e1674
ribaorsi <ribaorsi@gmail.com>
2022-04-26 00:11:15 +0200

    finished

--- reactinitial/src/App.jsx
+++ reactinitial/src/App.jsx
@@ -2,39 +2,61 @@ import React, { useEffect, useState } from "react"
 import Loader from "./components/LoadingMask";
 import axios from "axios";
 import Laptop from "./components/Laptop";
+import { TextField, Button } from "@mui/material";
+
 
 const App = () => {
 
  const [isLoading, setisLoading] = useState(true);
+ const [laptops, setLaptops] = useState([]);
 
  const [inputValue, setInputValue] = useState("");
+ const [isLowest, setIsLowest] = useState(true);
 
   const load = () => {
+    setisLoading(true);
     axios.get("https://demoapi.com/api/laptop")
     .then((response) => {
       setLaptops(response.data);
+      setisLoading(true);
     });
   }
-  useEffect(()=> {
-    load();    
-  },[]);
 
-  const sort = () =>{
+    useEffect(()=> {
+      load();    
+    },[]);
 
-  }
+    const sort = () =>{
+      if (isLowest) {
+      let newSort = laptops.sort((a,b) => (a.weigth > b.weigth ? 1 : -1));
+      setLaptops (newSort);
+      setIsLowest(false);
+      }
+      else {
+      let newSort = laptops.sort((a,b) => (b.weigth > a.weigth ? 1 : -1));
+      setLaptops (newSort);
+      setIsLowest(true);
+      }
+    };
+  
 
   return (
     <div>
       <h1>Laptops</h1>
       <header>
-      <button onClick={sort}>Sort</button><br></br>
-        <input 
+      <Button variant ="contained" onClick={sort}>Sort </Button><br></br>
+        <TextField 
         type="text" 
         value ={inputValue}
-        onChange={(e) => setInputValue(e.target.value)} />
+        onChange={(e) => {setInputValue(e.target.value)}} />
       </header>
       {isLoading && <Loader />}
-      {laptops.map(laptop => <Laptop laptop={laptop}/>)}
+      {laptops
+      .filter((data) => data.name.slice(0, inputValue.length).toLowerCase() === inputValue.toLowerCase())
+      .map((data,i) => 
+      <div key = {i}>
+        <Laptop key={i} laptop={data}/>
+      </div>)}
     </div>
   )
 }
--- reactinitial/src/components/Laptop.jsx
+++ reactinitial/src/components/Laptop.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useState } from "react";
+import { Button } from "@mui/material";
 
 const Laptop = ({laptop}) => {
   const [isShown, setIsShown] = useState(false);
   const toggle= () => {
     if(isShown===false) {setIsShown(true)}
-  }
+  
+  else {setIsShown(false)}
+}
     return (
       <div>
         <p>{laptop.name}</p>
-        {isShown && <p>{laptop.brand}</p> && <p>{laptop.weigth}</p>}
-        <button onClick={toggle}>{isShown ? "Show Less" : "Show more"}</button>
+        {isShown && <p>Product: {laptop.brand} <br></br>
+        Weight: {laptop.weigth}</p>}
+        {<Button variant = "contained" onClick={toggle}>{isShown ? "Show Less" : "Show more"}</Button>}
     </div>
   );
   }
